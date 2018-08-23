c45344bd228b901a4745db4581aec8cf26faf4e0
LP Boucher <lp_boucher@hotmail.com>
2018-08-23 22:15:53 +0200

    implemented constants file to move hardcoded strings away from logic

--- src/components/HowItWorks/index.js
+++ src/components/HowItWorks/index.js
@@ -1,40 +1,19 @@
 import React from 'react'
 import { Segment, Grid, Header, Icon } from 'semantic-ui-react'
+import { HOW_IT_WORKS } from '../../constants'
 
 const HowItWorks = () => (
   <Segment color="grey">
     <Grid>
       <Grid.Row columns={4} divided>
-        <Grid.Column>
-          <Header as="h2" icon text-align="center">
-            <Icon name="user circle" circular />
-            <Header.Content>
-              Register and choose a subscription level
-            </Header.Content>
-          </Header>
-        </Grid.Column>
-        <Grid.Column>
-          <Header as="h2" icon text-align="center">
-            <Icon name="check circle" circular />
-            <Header.Content>
-              Select your preferred coffee and delevery schedule
-            </Header.Content>
-          </Header>
-        </Grid.Column>
-        <Grid.Column>
-          <Header as="h2" icon text-align="center">
-            <Icon name="coffee" circular />
-            <Header.Content>
-              Enjoy your coffee delivered right to your door!
-            </Header.Content>
-          </Header>
-        </Grid.Column>
-        <Grid.Column>
-          <Header as="h2" icon text-align="center">
-            <Icon name="thumbs up" circular />
-            <Header.Content>Wait... and enjoy some more!</Header.Content>
-          </Header>
-        </Grid.Column>
+        {HOW_IT_WORKS.map(item => (
+          <Grid.Column key={item.id}>
+            <Header as="h2" icon text-align="center">
+              <Icon name={item.icon} circular />
+              <Header.Content>{item.explainText}</Header.Content>
+            </Header>
+          </Grid.Column>
+        ))}
       </Grid.Row>
     </Grid>
   </Segment>
---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const HOW_IT_WORKS = [
+  {
+    explainText: 'Register and choose a subscription level',
+    icon: 'user circle',
+    id: 'HITW1',
+  },
+  {
+    explainText: 'Select your preferred coffee and delevery schedule',
+    icon: 'check circle',
+    id: 'HITW2',
+  },
+  {
+    explainText: 'Enjoy your coffee delivered right to your door!',
+    icon: 'coffee',
+    id: 'HITW3',
+  },
+  {
+    explainText: 'Wait... and enjoy some more!',
+    icon: 'thumbs up',
+    id: 'HITW4',
+  },
+]
